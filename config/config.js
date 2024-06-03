66c170c154ef85b7c63327abc9f1518e5ed74313
Aastha Singh Sachan <98797127+Aastha-Singh-111@users.noreply.github.com>
2024-06-03 16:07:27 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const Port = process.env.NODE_ENV === 'test' ? 4001 : (process.env.PORT || 4000);
 
-const MongodbUrl =  process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_PRODUCTION : (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb://127.0.0.1:27017/medbuddy')
+const MongodbUrl =  process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_PRODUCTION : (process.env.MONGODB_URL_DEVELOPMENT || 'mongodb+srv://aasthasinghclg4:Aastha123@clustermed.5apg3ff.mongodb.net/')
 
 module.exports = {
   Port: Port,
